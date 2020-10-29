c38d2023d8e0832cadca6c70c528d821c5faf1f6
RyanRaffoul <72235540+RyanRaffoul@users.noreply.github.com>
2020-10-29 09:41:53 -0400

    Update JobCreatorServerThread.java

--- Assignments/Assignment2/JobCreatorServerThread.java
+++ Assignments/Assignment2/JobCreatorServerThread.java
@@ -1,4 +1,9 @@
-// Put Group and Assignment info here
+// // Assignment 2: Job Creator Server Thread
+// Ryan Raffoul
+// JobCreatorServerThread.java
+// This class simply communicates with the Job Seeker to complete the task and terminate the connection
+
+// libraries used
 import java.io.*;
 import java.util.*;
 import java.net.*;
