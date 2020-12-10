55a72491b41d84b5210d47d4117609a0167b644d
ryanr <ryanr@DESKTOP-2T7ALDT.cogeco.local>
2020-12-10 14:09:19 -0500

    Fixing error

--- Project/Server.java
+++ Project/Server.java
@@ -11,14 +11,15 @@ public class JobCreatorServer
 		
 		try(ServerSocket serverSocket = new ServerSocket(port)){
 		
+			// outputs to display what the Server does
 			System.out.println("This is a Network Discovery Tool\n");
-			System.out.println("Server is available at Port " +port + " looking for a Client to get a Hostname (Not IP Address) do the following: ")
+			System.out.println("Server is available at Port " +port + " looking for a Client to get a Hostname (Not IP Address) do the following: ");
 			System.out.println("Do a WHOIS Lookup for Hostnames that qualify");
 			System.out.println("Find all IP Addresses for this Hostname");
 			System.out.println("For all IP Addresses found: Check if the IP is reachable and find all Open Ports\n");
 		
 			while(true) {
-				// accept and send
+				// accept and send to the Server Thread
 				Socket socket = serverSocket.accept();
 				System.out.println("\nConnected to a Client\n");
 				new JobCreatorServerThread(socket).start();
